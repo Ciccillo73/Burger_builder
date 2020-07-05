dc403c8ab35b831510ea2052b38d45f75d853942
Francesco <med.dama@gmail.com>
2020-07-05 15:05:16 +0100

    Add hihger folder component for Appp layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <p>Burger Builder</p>
+      <div>
+        
+
+      </div>
     );
   }
 }
