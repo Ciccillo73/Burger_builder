1ac48b989cfd56b4b6126f6775bed26bc1a7bee6
Francesco <med.dama@gmail.com>
2020-07-05 18:18:45 +0100

    Adds burger ingredients render

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,17 @@
 import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.css";
-import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
 const burger = (props) => {
     return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="bread-meat"/>
-            <BurgerIngredient type="bread-cheese"/>
-            <BurgerIngredient type="bread-bottom"/>
-        </div>
+      <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top" />
+        <BurgerIngredient type="cheese" />
+        <BurgerIngredient type="meat" />
+        <BurgerIngredient type="salad" />
+
+        <BurgerIngredient type="bread-bottom" />
+      </div>
     );
 };
 
