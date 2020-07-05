233386f1a9e60b82579e51ffdc000093292d71e5
Francesco <med.dama@gmail.com>
2020-07-05 17:41:17 +0100

    Convert functional component to a class

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,43 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
 import classes from "./BurgerIngredient.css";
+import PropTypes from "prop-types";
 
-const burgerIngredient = (props) => 
-{
-    let ingredient = null;
+class BurgerIngredient extends Component {
+    render(){
 
-    switch (props.type) {
-        case ("bread-bottom"):
+        let ingredient = null;
+
+        switch (props.type) {
+          case "bread-bottom":
             ingredient = <div className={classes.BreadBottom}></div>;
             break;
-        case ("bread-top"):
+          case "bread-top":
             ingredient = (
-                <div className={classes.BreadTop}>
-                    <div className={classes.Seeds1}></div>
-                    <div className={classes.Seeds2}></div>
-                </div>
+              <div className={classes.BreadTop}>
+                <div className={classes.Seeds1}></div>
+                <div className={classes.Seeds2}></div>
+              </div>
             );
             break;
-        case ("meat"):
-            ingredient = 
-                <div className={classes.Meat}></div>;
+          case "meat":
+            ingredient = <div className={classes.Meat}></div>;
             break;
-        case ("cheese"):
-            ingredient = 
-                <div className={classes.Cheese}></div>;
+          case "cheese":
+            ingredient = <div className={classes.Cheese}></div>;
             break;
-        case ("salad"):
-            ingredient = 
-                <div className={classes.Meat}></div>;
+          case "salad":
+            ingredient = <div className={classes.Meat}></div>;
             break;
-        case ("bacon"):
-            ingredient = 
-                 <div className={classes.Meat}></div>;
+          case "bacon":
+            ingredient = <div className={classes.Meat}></div>;
             break;
-        default:
+          default:
             ingredient = null;
+        }
+        return ingredient;
     }
-    return ingredient;
+} 
 
-};
 
 export default burgerIngredient;
