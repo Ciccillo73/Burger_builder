c594bb9f3b780855a301e5e714832c04d2cb520b
Francesco <med.dama@gmail.com>
2020-07-05 18:08:16 +0100

    Render the Burger ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
-
-
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = (props) => {
     return (
-        <div className="">
-            <BurgerIngredient />
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="bread-meat"/>
+            <BurgerIngredient type="bread-cheese"/>
+            <BurgerIngredient type="bread-bottom"/>
         </div>
     );
 };
