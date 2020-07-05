491e11fe5b72a35387267d0949bed108113371df
Francesco <med.dama@gmail.com>
2020-07-05 15:12:30 +0100

    Export layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Aux from "../../hoc/Aux";
 
-
-const layout = (props) => {
-
+const layout = (props) => (
+    <Aux>
    <div>   
         Toolbar, SideDrawer, Backdrop
     </div>
     <main>
         {props.children}
     </main>
+    </Aux>
 
+);
 
-}
+export default layout;
