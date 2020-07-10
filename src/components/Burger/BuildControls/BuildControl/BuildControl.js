6c2424057231fca40cae236408852edd77526c24
Francesco <med.dama@gmail.com>
2020-07-10 15:58:15 +0100

    Add style to Html

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,11 @@
 import React from "react";
 import buildControls from "../BuildControls";
-
+import class from "./BuildControl.css";
 const buildControl = (props) => (
-  <div>
-    <div>{props.ingredients}</div>
-    <button>Less</button>
-    <button>More</button>
+  <div className={classes.BuildControl}>
+    <div className={classes.label}>{props.label}</div>
+    <button className={classes.less}>Less</button>
+    <button className={classes.More}>More</button>
   </div>
 );
 
