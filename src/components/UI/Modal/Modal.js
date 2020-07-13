76a41e98c2e8864aa37078deb04bf5302090fc5f
Francesco <med.dama@gmail.com>
2020-07-13 13:09:38 +0100

    Add purchaseCancelHandler function

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from "./Modal.css";
-import Aux from '../../../hoc/Aux';
-import Backdrop from '../BackDrop/Backdrop';
+import Aux from "../../../hoc/Aux";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = (props) => (
   <Aux>
-      <Backdrop show={props.show} />  
+    <Backdrop show={props.show} clicked={props.modalClosed}/>
     <div
       className={classes.Modal}
       style={{
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -71,6 +71,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: true });
   };
 
+  puchaseCancelHandler = () => {
+    this, this.setState({ purchasing: false });
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
@@ -80,7 +84,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
