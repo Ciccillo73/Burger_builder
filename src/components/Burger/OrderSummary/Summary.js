9e29afbe33a08ff0336282e0ffd5cd64abf2ed07
Francesco <med.dama@gmail.com>
2020-07-13 11:43:02 +0100

    Render the ingredient summary

--- src/components/Burger/OrderSummary/Summary.js
+++ src/components/Burger/OrderSummary/Summary.js
@@ -15,8 +15,9 @@ const orderSummary = (props) => {
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
-
+                {ingredientSummary}
             </ul>
+            <p>Continue to Checkout</p>
         </Aux>
     )
 };
