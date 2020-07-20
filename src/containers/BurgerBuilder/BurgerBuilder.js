b74474a4a02fcab9006088b64130a604f7632f76
Francesco <med.dama@gmail.com>
2020-07-20 15:58:44 +0100

    HAdle componentDidMount

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,6 +22,7 @@ class BurgerBuilder extends Component {
     purchasable: false,
     purchasing: false,
     loading: false,
+    error: false
   };
 
   componentDidMount() {
@@ -29,7 +30,12 @@ class BurgerBuilder extends Component {
       .get("https://react-my-burger-27b83.firebaseio.com/ingredients.json")
       .then((response) => {
         this.setState({ ingredients: response.data });
+      })
+      .catch(error => {
+        this.setState({error: true})
       });
+
+      
   }
 
   updatePurchaseState(ingredients) {
@@ -115,7 +121,7 @@ class BurgerBuilder extends Component {
     }
     let orderSummary = null;
 
-    let burger = <Spinner />;
+    let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner />;
 
     if (this.state.ingredients) {
       burger = (
