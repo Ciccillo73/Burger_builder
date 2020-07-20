c199f704788bc97e99c70ae2b181ad90a0a5be7e
Francesco <med.dama@gmail.com>
2020-07-20 12:47:07 +0100

    Fixed the totalPrice decimals

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -79,7 +79,7 @@ class BurgerBuilder extends Component {
   purchaseContinueHandler = () => {
     const order = {
       ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
+      price: this.state.totalPrice.toFixed(2),
       customer: {
         name: "Max Schrzm",
         address: {
@@ -92,11 +92,11 @@ class BurgerBuilder extends Component {
       deliveryMethod: "fastest",
     };
     //Firebase connection
-    
-    // axios
-    //   .post("/orders.json", order)
-    //   .then((response) => console.log(response))
-    //   .catch((error) => console.log(error));
+
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
