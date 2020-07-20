f6fa8ce3df7ea1ff32b508d422eb79241f5de4a5
Francesco <med.dama@gmail.com>
2020-07-20 12:21:31 +0100

    Import axios in BurgerBuilder component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls.js";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
