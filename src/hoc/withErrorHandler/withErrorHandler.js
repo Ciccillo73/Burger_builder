75520858ac2e4db588b49489fa5bc1e7f1f00577
Francesco <med.dama@gmail.com>
2020-07-20 14:42:44 +0100

    Add error in Modal

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-import Modal from "../../components/UI/Modal/Modal";
-import Aux from "../Aux/Aux";
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Aux/Aux';
 
-const withErrorHandler = (WrappedComponent) => {
-  return (props) => {
-    return (
-      <Aux>
-        <Modal>Something didn't work!</Modal>
-        <WrappedComponent {...props} />
-      </Aux>
-    );
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+        error: null
+    }      
+    componentDidMount () {
+        axios.interceptors.request.use(req => {
+            this.setState({error: null})
+        });
+          axios.interceptors.response.use(null, error => {
+            this.setState({error: error})
+          });
+      }
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error}>{this.state.eror.message}</Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
   };
 };
-
 export default withErrorHandler;
